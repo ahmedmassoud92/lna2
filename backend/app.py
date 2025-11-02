5fd8201e2540493a41242d2e34080544fa514a96
ahmedmassoud92 <ahmed.massoud.abd@gmail.com>
2025-11-02 02:21:53 +0200

    Set debug mode to False in app.run
    Changed debug mode to False for production.

--- backend/app.py
+++ backend/app.py
@@ -171,4 +171,5 @@ def api_search():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
-    app.run(host="0.0.0.0", port=port, debug=True)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 8000)), debug=False)
+
